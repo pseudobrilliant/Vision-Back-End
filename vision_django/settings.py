743fce35825a8a153fa04322d10bd69ee5ec92bd
Christian Blythe <ucfblythe83@gmail.com>
2018-11-18 16:00:47 -0500

    adding host

--- vision_django/settings.py
+++ vision_django/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vision-project.herokuapp.com']
 
 
 # Application definition
